bb14f5a7d5c5ee17afd0fa856c10f1be8b0b95c9
kangdain <juhyueon3@naver.com>
2020-12-14 04:15:27 +0900

    like 버튼 클릭시 mutation 된 데이터가 최초 작동시 ui에반영되지않는 버그가 있음

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,31 @@
-import { ApolloClient , InMemoryCache}  from "@apollo/client";
+import { ApolloClient , InMemoryCache, gql}  from "@apollo/client";
+
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.writeQuery({
+          query: gql`
+          {
+            id
+            isLiked
+          }
+        `,
+          id: `Movie:${id}`,
+
+          data: {
+            isLiked: !isLiked
+          }
+        });
+      }
+    }
+  }
 });
 
 export default client;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,11 +6,17 @@ import styled from "styled-components";
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
+      id
       title
       medium_cover_image
       language
       rating
       description_intro
+      isLiked @client
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
     }
   }
 `;
@@ -56,24 +62,23 @@ const Poster = styled.div`
 export default () => {
   const { id } = useParams();
   const { loading, data } = useQuery(GET_MOVIE, {
-    variables: { id }
+    variables: { id: parseInt(id) }
   });
   return (
     <Container>
       <Column>
-        <Title>{loading ? "Loading..." : data.movie.title}</Title>
-        {!loading && data.movie && (
-          <>
-            <Subtitle>
-              {data.movie.language} · {data.movie.rating}
-            </Subtitle>
-            <Description>{data.movie.description_intro}</Description>
-          </>
-        )}
+        <Title>
+          {loading
+            ? "Loading..."
+            : `${data.movie.title} ${data.movie.isLiked ? "💖" : "😞"}`}
+        </Title>
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
       </Column>
-      <Poster
-        bg={data && data.movie ? data.movie.medium_cover_image : ""}
-      ></Poster>
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
     </Container>
   );
 };
+
